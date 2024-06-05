53b24c9be4eb4274d18723f090420a871d272c58
Hadar Yeheskiel <hadaryeheskiel@Hadars-MacBook-Air.local>
2024-06-05 21:45:06 +0300

    home page

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import { react } from 'react';
+import './Home.scss';
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <h3 className='main-header'>create your own</h3>
+            <div>
+                <div className='card'>
+                    <div className='image bracelet'></div>
+                    <span>צמידים</span>
+                </div>
+                <div className='card'>
+                    <div className='image necklace'></div>
+                    <span>שרשראות</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import { react } from 'react';
+import './NavBar.scss';
+
+const NavBar = () => {
+    return (
+        <div className='nav-bar'>
+            <h1 className='logo-header'>Mona</h1>
+        </div>
+    )
+}
+
+export default NavBar;
